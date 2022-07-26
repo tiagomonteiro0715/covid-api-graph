f122d68a6ecc6be4b30c64f91d5a9d62380fe4bb
Tiago monteiro <tiago.monteiro0715@gmail.com>
2022-07-26 11:24:55 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#tutorial https://towardsdatascience.com/json-and-apis-with-python-fba329ef6ef0~
+
+import requests
+import pandas as pd
+
+response_info = requests.get("https://api.covid19api.com/summary").json()
+
+country_list = []
+for country in response_info["Countries"]:
+    country_list.append([country["Country"], country["TotalConfirmed"], country["TotalDeaths"]])
+
+df = pd.DataFrame(data=country_list, columns=["Country", "Total_Confirmed", "TotalDeaths"])
+
+df.to_csv("country.csv")
